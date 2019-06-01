2419948e1cfc67d810816e009cc5208e2d3e7c0c
BohdanSerdyuk <bohdanserdyuk@gmail.com>
2019-06-01 19:04:58 +0300

    gap commit of FeignClient

--- feign-service/src/main/java/com/example/demo/FeignServiceApplication.java
+++ feign-service/src/main/java/com/example/demo/FeignServiceApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
-public class ServicesApplication {
+public class FeignServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServicesApplication.class, args);
+        SpringApplication.run(FeignServiceApplication.class, args);
     }
 
 }
--- feign-service/src/test/java/com/example/demo/FeignServiceApplicationTests.java
+++ feign-service/src/test/java/com/example/demo/FeignServiceApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServicesApplicationTests {
+public class FeignServiceApplicationTests {
 
     @Test
     public void contextLoads() {
