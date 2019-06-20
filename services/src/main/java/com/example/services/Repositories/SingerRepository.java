1c1d8f4f1298ecfc940e55814d43f85e35bcd21c
BohdanSerdyuk <bohdanserdyuk@gmail.com>
2019-06-20 11:56:39 +0300

    docker composer and properties created

--- services/src/main/java/com/example/services/Repositories/SingerRepository.java
+++ services/src/main/java/com/example/services/Repositories/SingerRepository.java
@@ -2,6 +2,8 @@ package com.example.services.Repositories;
 
 import com.example.services.Models.Singer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SingerRepository extends JpaRepository<Singer, Integer> {
 }
--- services/src/main/java/com/example/services/Repositories/TrackRepository.java
+++ services/src/main/java/com/example/services/Repositories/TrackRepository.java
@@ -2,6 +2,8 @@ package com.example.services.Repositories;
 
 import com.example.services.Models.Track;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TrackRepository extends JpaRepository<Track, Integer> {
 }
